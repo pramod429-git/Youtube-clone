0b881d6987f9d95bead6f31318bd32e6c1575974
pramod429-git <18kppramod@gmail.com>
2025-03-05 15:23:14 +0530

    youtube secret key

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 export const YOUTUBE_API_VIDEO =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
